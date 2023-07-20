eac078d979966e6c942fd839aa396ad27d9f398c
Mohammadmahdi <m.mahdi.nayyeri1383@gmail.com>
2023-07-20 16:57:22 +0330

    second change

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Product from './Product/Product';
+import img1 from './images/images.jpg';
+import img2 from './images/imagesds.jpg';
+import img3 from './images/Untitled.png';
 
 function App() {
 
   const allProducts=[
-    {id:1, title: 'aaa', image: "images/Untitled.png", price: 230, count: 20},
-    {id:1, title: 'abb', image: 'images/images.jpg', price: 130, count: 50},
-    {id:1, title: 'abc', image: 'images/imagesds.jpg', price: 30, count: 2},
+    {id:1, title: 'aaa', image: img1 , price: 230, count: 20},
+    {id:1, title: 'abb', image: img2 , price: 130, count: 50},
+    {id:1, title: 'abc', image: img3 , price: 30, count: 2},
   ]
   return (
     <div className='container'>
